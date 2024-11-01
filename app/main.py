d92b7aa161d73ec136d7e4cbb3ace55ca20077cf
qt <qtbytes@outlook.com>
2024-11-01 14:36:24 +0800

    Response to client PING

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    command = server_socket.accept()  # wait for client
-    print(f"recieve command {command}")
+    conn, addr = server_socket.accept()  # wait for client
+    print(f"recieve client {conn} {addr}")
     response = b"+PONG\r\n"
-    print(response)
-    server_socket.sendall(response)
+    print(f"sending response {response}")
+    conn.sendall(response)
 
 
 if __name__ == "__main__":
