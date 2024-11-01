32bb7453cf0503d268ac6b6254d772cca45cb293
qt <qtbytes@outlook.com>
2024-11-01 14:38:27 +0800

    Respong to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -5,9 +5,12 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, addr = server_socket.accept()  # wait for client
     print(f"recieve client {conn} {addr}")
-    response = b"+PONG\r\n"
-    print(f"sending response {response}")
-    conn.sendall(response)
+    while True:
+        command = conn.recv()
+        print(command)
+        response = b"+PONG\r\n"
+        print(f"sending response {response}")
+        conn.sendall(response)
 
 
 if __name__ == "__main__":
