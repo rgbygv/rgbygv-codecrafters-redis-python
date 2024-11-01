e3bccc336abd9622931642a982a6bdfd50824a6b
vthe <idashatao@gmail.com>
2024-11-01 14:14:02 +0800

    Respond to PING

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ import socket  # noqa: F401
 
 
 def main():
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept()  # wait for client
+
+    server_socket.send("+PONG\r\n")
 
 
 if __name__ == "__main__":
