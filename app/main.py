c87c90a7c67518263039b03b9f99f56bdaba31ab
qt <qtbytes@outlook.com>
2024-11-01 14:22:05 +0800

    Respond to PING

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
-
-    server_socket.send(b"+PONG\r\n")
+    command = server_socket.accept()  # wait for client
+    print(f"recieve command {command}")
+    if command == "PING":
+        server_socket.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
