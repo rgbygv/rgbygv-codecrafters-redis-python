dfbbf51d1b02d31ba9d2f90e566e174a03c49020
qt <qtbytes@outlook.com>
2024-11-01 15:19:01 +0800

    Handle concurrnet clients by asyncio

--- app/main.py
+++ app/main.py
@@ -5,22 +5,24 @@ import asyncio
 async def handle_client(conn: socket, addr: str):
     print(f"Accepted connection from {addr}")
     while True:
-        msg = await conn.recv(1024)
+        msg = conn.recv(1024)
         if not msg:
             break
         print(f"Received: {msg}")
         response = b"+PONG\r\n"
         print(f"Sending response {response}")
-        await conn.sendall(response)
+        conn.sendall(response)
     conn.close()
     print(f"Closes connection from {addr}")
 
 
 async def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.setblocking(False)
     loop = asyncio.get_event_loop()
     while True:
         conn, addr = await loop.sock_accept(server_socket)
+        conn.setblocking(False)
         loop.create_task(handle_client(conn, addr))
 
 
