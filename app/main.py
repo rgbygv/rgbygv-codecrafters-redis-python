2f8d4a7978f741ed6a54a9eda3ac3a16dfe84f36
qt <qtbytes@outlook.com>
2024-11-01 14:54:16 +0800

    Handle concurrent clients by asyncio

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 import socket  # noqa: F401
+import asyncio
+
+
+async def handle_client(s: socket):
+    conn, addr = s.accept()  # wait for client
+    print(f"Accepted connection from {addr}")
+    while True:
+        msg = await conn.recv(1024)
+        print(msg)
+        response = b"+PONG\r\n"
+        print(f"sending response {response}")
+        conn.sendall(response)
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
-    print(f"recieve client {conn} {addr}")
-    while True:
-        command = conn.recv(1024)
-        print(command)
-        response = b"+PONG\r\n"
-        print(f"sending response {response}")
-        conn.sendall(response)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(handle_client(server_socket))
 
 
 if __name__ == "__main__":
