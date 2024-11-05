b7e497e556fa02f3a873539114aea5be46b907ed
qt <qtbytes@outlook.com>
2024-11-05 21:55:20 +0800

    fix: don't send ack to replica

--- app/main.py
+++ app/main.py
@@ -142,9 +142,9 @@ async def handle_command(msg: bytes, connection_port: str | None, reader, writer
         for _replica_port, _writer in r.connect_replica.values():
             print(f"Propagating command {msg} to replica {_replica_port}")
             await send_command_to_replica(_replica_port, _writer, msg)
-            print(f"Sending acknowledgement to replica {_replica_port}")
-            ack_msg = encode(b"REPLCONF GETACK *".split())
-            await send_command_to_replica(_replica_port, _writer, ack_msg)
+            # print(f"Sending acknowledgement to replica {_replica_port}")
+            # ack_msg = encode(b"REPLCONF GETACK *".split())
+            # await send_command_to_replica(_replica_port, _writer, ack_msg)
         if len(args) == 2:
             k, v = args
             r.m[k] = encode([v])
